9d618c32ace9beecd25a0ca260c2d97b01420720
suyog ojha <ojhasuyog2020@gmail.com>
2022-07-18 15:36:01 +0545

    state added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,23 @@
 
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props){
 
+    const [title, setTitle] = useState(props.title);
+
+    const ClickHandler = () => {
+        setTitle("New title added");
+    }
     return(
     <div className='expense-item'>
             <ExpenseDate date= {props.date} />
             <div className='expense-item_description'>
-                <h2>{ props.title }</h2>
+                <h2>{ title }</h2>
                 <h2 className='expense-item_price'> { props.amount } </h2>
-
+                <button onClick={ ClickHandler }> Click Me</button>
             </div>
            </div> 
     );
